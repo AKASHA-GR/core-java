01fec9baf680c519f923d2017fd13f8d25b116ff
AKASHA-GR <akashmlk2021@gmail.com>
2026-02-05 23:40:00 +0530

    Add files via upload

---
+++ Task_6(Array)/ArrayName.java
@@ -0,0 +1,51 @@
+//declare 15 Arrays 
+
+class ArrayName{
+	public static void main(String[] args){
+		
+		String footBallPlayer[]={"Akasha GR", "Rahul CB", "Nagaprajwal B", "Raghunandan", "Varun ML"};
+		System.out.println(footBallPlayer[2]);
+		
+		byte values[] = {-88, -27, 78, 23, 126};
+		System.out.println(values[4]);
+		
+		int numbers[] = {10, 20 , 30, 40, 50};
+		System.out.println(numbers[0]);
+		
+		float temparature[] = {9.1f, 13.6f, 7.6f, 8.2f, 4.4f};
+		System.out.println(temparature[1]);
+		
+		double persentage[] = {6.453245, 8.432823, 1.842404, 3.229222};
+		System.out.println(persentage[2]);
+		
+		short celcius[] = {-1000, -130, -107, -8, 0, 12, 42};
+		System.out.println(celcius[0]);
+		
+		long phoneNumber[] = {27392492l, 82447292l, 49632994l, 27352272l};
+		System.out.println(phoneNumber[3]);
+		
+		char letter[] = {'A', 'c', 'K', '%', 'Q', '$'};
+		System.out.println(letter[5]);
+		
+	    boolean isPass[] = {false, true};
+		System.out.println(isPass[1]);
+		
+		String animals[] = {"Cat", "Dog", "Cattle", "insects", "Snake"};
+		System.out.println(animals[4]);
+		
+		String clothing[] = {"Jacket", "Shorts", "Blouse", "Snekers", "Skirt"};
+		System.out.println(clothing[0]);
+		
+		String meterials[] = {"Bricks", "Cement", "Sand", "Glass", "Wood", "Plastic", "Reinforcement"};
+		System.out.println(meterials[6]);
+		
+		String hobbies[] = {"Journaling", "Practice", "Do Puzzles", "Swimming", "Cooking", "Walking", "Reading", "Coloring"};
+		System.out.println(hobbies[6]);
+		
+		long pinNumber[] = {28934l, 23792l, 22379l, 47239l, 39780l, 17593l, 18627l, 57896l,};
+		System.out.println(pinNumber[7]);
+		
+		float Gravity[] = {7.84f, 9.12f, 7.36f, 63.89f};
+		System.out.println(Gravity[2]);
+	}
+}
