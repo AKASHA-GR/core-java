99602d3e33d8f81e9d1c924649e591fb74ee16b8
Akasha G R <akashmlk2021@gmail.com>
2026-02-19 12:23:29 +0530

    new keyword

---
+++ ClassesRunner.java
@@ -0,0 +1,60 @@
+class ClassesRunner{
+	public static void main(String[] args){
+		Light light = new Light();
+		System.out.println("The first copy.");
+		Light light1 = new Light();
+		System.out.println("The Second copy.");
+		
+		Dress dress = new Dress();
+		Dress dress1 = new Dress();
+		
+		Disc disc = new Disc();
+		Disc disc1 = new Disc();
+		
+		Button button = new Button();
+		Button button1 = new Button();
+		
+		Chocolate chocolate = new Chocolate();
+		Chocolate chocolate1 = new Chocolate();
+		
+		Logo logo = new Logo();
+		Logo logo1 = new Logo();
+		
+		Coin coin = new Coin();
+		Coin coin1 = new Coin();
+		
+		Borewell borewell = new Borewell();
+		Borewell borewell1 = new Borewell();
+		
+		Robot robot = new Robot();
+		Robot robot1 = new Robot();
+		
+		Iron iron = new Iron();
+		Iron iron1 = new Iron();
+		
+		Box box = new Box();
+		Box box1 = new Box();
+		
+		Wood wood = new Wood();
+		Wood wood1 = new Wood();
+		
+		Plastic plastic = new Plastic();
+		Plastic plastic1 = new Plastic();
+		
+		Number number = new Number();
+		Number number1 = new Number();
+		
+		SkateBoard = new skateoard();
+		SkateBoard1 = new skateoard();
+		
+		ToothPaste toothPaste = new toothPaste();
+		ToothPaste toothPaste1 = new toothPaste();
+		
+		Tom tom = new Tom();
+		Tom tom1 = new Tom();
+		
+		Jerry jerry = new Jerry();
+		Jerry jerry1    = new Jerry();
+		
+	}
+}
